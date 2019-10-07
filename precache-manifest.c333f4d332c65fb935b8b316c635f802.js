36fbca6e2b146c283295d11d99e7e77e0c1f4687
Alex <alexroch07@gmail.com>
2019-10-07 18:41:33 -0400

    Updates

---
+++ precache-manifest.c333f4d332c65fb935b8b316c635f802.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4308492ccde7afba1d0a65fecd3a0e4c",
+    "url": "/bike-site/index.html"
+  },
+  {
+    "revision": "7d1764ec1c624af81d06",
+    "url": "/bike-site/static/css/main.5f3c2cd5.chunk.css"
+  },
+  {
+    "revision": "1f9b71f3a997f8d5a948",
+    "url": "/bike-site/static/js/2.2cc7bbe2.chunk.js"
+  },
+  {
+    "revision": "7d1764ec1c624af81d06",
+    "url": "/bike-site/static/js/main.be91ee2e.chunk.js"
+  },
+  {
+    "revision": "aff971d8069dad013480",
+    "url": "/bike-site/static/js/runtime-main.6934beaf.js"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/bike-site/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "7582e823ef0d702969ea0cce9afb326d",
+    "url": "/bike-site/static/media/Lato-Italic.7582e823.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/bike-site/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/bike-site/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "2af050dbfb5dd3c99a8fefceb4d02ba4",
+    "url": "/bike-site/static/media/bg-bike1.2af050db.png"
+  },
+  {
+    "revision": "393d154b19c6636a474772295c0a9c7d",
+    "url": "/bike-site/static/media/bg-bike2.393d154b.png"
+  },
+  {
+    "revision": "27affaea0683e7ce8276c11b67bfb289",
+    "url": "/bike-site/static/media/bg-bike3.27affaea.png"
+  },
+  {
+    "revision": "4aafff40263c91b569b7559e51250d01",
+    "url": "/bike-site/static/media/bg-top.4aafff40.png"
+  },
+  {
+    "revision": "025ac48371ffe38fba414b8a7af63870",
+    "url": "/bike-site/static/media/img-bike.025ac483.png"
+  },
+  {
+    "revision": "35a2eca0ec07cb9b6ea9472420fd50c4",
+    "url": "/bike-site/static/media/img-bike1.35a2eca0.png"
+  },
+  {
+    "revision": "b7c4af3f6fc37839e05eb4d1e8bc4bee",
+    "url": "/bike-site/static/media/img-bike2.b7c4af3f.png"
+  },
+  {
+    "revision": "7be15e5c360878313ddd5d151e1f4f67",
+    "url": "/bike-site/static/media/img-bike3.7be15e5c.png"
+  },
+  {
+    "revision": "acd682dd9dabd8abaefc67fecda63fb2",
+    "url": "/bike-site/static/media/prod1.acd682dd.png"
+  },
+  {
+    "revision": "15f6aaa3e81b3f890f7fc96097fde6c3",
+    "url": "/bike-site/static/media/prod2.15f6aaa3.png"
+  },
+  {
+    "revision": "bf5b666e4c5f039426634d488b017613",
+    "url": "/bike-site/static/media/prod3.bf5b666e.png"
+  },
+  {
+    "revision": "9ec651553ec6419afb5bec8aadfc7a4e",
+    "url": "/bike-site/static/media/prod4.9ec65155.png"
+  },
+  {
+    "revision": "ea19e9ffc0669f0860e9d45b5e16e9fc",
+    "url": "/bike-site/static/media/prod5.ea19e9ff.png"
+  },
+  {
+    "revision": "447e6b7f5a9febd7775061b84877d280",
+    "url": "/bike-site/static/media/prod6.447e6b7f.png"
+  },
+  {
+    "revision": "eb3e4340c8bf735b0358e4879f859230",
+    "url": "/bike-site/static/media/prod7.eb3e4340.png"
+  },
+  {
+    "revision": "6fc78b7d74b151f091300776580552c4",
+    "url": "/bike-site/static/media/prod8.6fc78b7d.png"
+  },
+  {
+    "revision": "6f35b8560c6de0d92d567ca3eb16978e",
+    "url": "/bike-site/static/media/toggle.6f35b856.svg"
+  }
+]);
